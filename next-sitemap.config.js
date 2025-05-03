bdc1caf1080863623db8da09204e34b6be3d9dcf
Falih <falihdzakwan028@gmail.com>
2025-05-03 22:03:48 +0700

    sitemap url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://your-domain.com",
+    siteUrl: "https://quotive.vercel.app",
     generateRobotsTxt: true,
 };
